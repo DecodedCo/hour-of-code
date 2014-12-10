51f1212966a6a67ce41cc71c9563d2f1d23a8669
Amadeus Stevenson <amadeus@decoded.us>
2014-12-10 14:39:02 -0500

    Reindent

--- snow.js
+++ snow.js
@@ -23,68 +23,68 @@ function startSnowing(flakes, interval) {
       y: Math.random()*H, //y-coordinate
       r: Math.random()*4+1, //radius
       d: Math.random()*mp //density
-	  })
-	}
+    })
+  }
 
-	//animation loop
-	setInterval(drawSnowflakes, interval);
+  //animation loop
+  setInterval(drawSnowflakes, interval);
 }
 
 //Lets draw the flakes
 function drawSnowflakes()
 {
-	ctx.clearRect(0, 0, W, H);
+  ctx.clearRect(0, 0, W, H);
 
-	ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-	ctx.beginPath();
-	for(var i = 0; i < mp; i++)
-	{
-	  var p = particles[i];
-	  ctx.moveTo(p.x, p.y);
-	  ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
-	}
-	ctx.fill();
-	updateSnowflakes();
+  ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+  ctx.beginPath();
+  for(var i = 0; i < mp; i++)
+  {
+    var p = particles[i];
+    ctx.moveTo(p.x, p.y);
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+  }
+  ctx.fill();
+  updateSnowflakes();
 }
 
 //Function to move the snowflakes
 function updateSnowflakes()
 {
-	angle += 0.01;
-	for(var i = 0; i < mp; i++)
-	{
-	  var p = particles[i];
-	  //Updating X and Y coordinates
-	  //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
-	  //Every particle has its own density which can be used to make the downward movement different for each flake
-	  //Lets make it more random by adding in the radius
-	  p.y += Math.cos(angle+p.d) + 1 + p.r/2;
-	  p.x += Math.sin(angle) * 2;
+  angle += 0.01;
+  for(var i = 0; i < mp; i++)
+  {
+    var p = particles[i];
+    //Updating X and Y coordinates
+    //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+    //Every particle has its own density which can be used to make the downward movement different for each flake
+    //Lets make it more random by adding in the radius
+    p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+    p.x += Math.sin(angle) * 2;
 
-	  //Sending flakes back from the top when it exits
-	  //Lets make it a bit more organic and let flakes enter from the left and right also.
-	  if(p.x > W+5 || p.x < -5 || p.y > H)
-	  {
-	    if(i%3 > 0) //66.67% of the flakes
+    //Sending flakes back from the top when it exits
+    //Lets make it a bit more organic and let flakes enter from the left and right also.
+    if(p.x > W+5 || p.x < -5 || p.y > H)
+    {
+      if(i%3 > 0) //66.67% of the flakes
       {
         particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
-			}
-			else
-			{
-				//If the flake is exitting from the right
-				if(Math.sin(angle) > 0)
-				{
-					//Enter from the left
-					particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
-				}
-				else
-				{
-					//Enter from the right
-					particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
-				}
-			}
-		}
-	}
+      }
+      else
+      {
+        //If the flake is exitting from the right
+        if(Math.sin(angle) > 0)
+        {
+          //Enter from the left
+          particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+        }
+        else
+        {
+          //Enter from the right
+          particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+        }
+      }
+    }
+  }
 }
 
 // UNCOMMENT BELOW TO START SNOWING
